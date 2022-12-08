b17eafdbd889b1cfb4e0b1c668373f34194b4396
luc-nguyen'174 <louiskingvn@gmail.com>
2022-12-08 15:08:41 +0700

    baitap

---
+++ bai-6/demo-js.js
@@ -0,0 +1,13 @@
+function mouseover() {
+        document.getElementById("demo").style.color="red";
+}
+function mouseout() {
+    document.getElementById("demo").style.color="yellow";
+}
+function change() { //ding nghia ham`
+    var selectValue=document.getElementById("demo").value;  //khai bao bien selectValue de luu value co id la` demo
+    alert("oke") //dua ra alert sau khi chay script
+}
+function onlick(){
+    alert("oke") //dua ra alert sau khi chay script
+}
