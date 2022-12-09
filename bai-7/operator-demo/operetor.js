e9e08f44111de49486f8fbaee79e00afe06e9109
luc-nguyen'174 <louiskingvn@gmail.com>
2022-12-09 16:24:13 +0700

    baitap

--- bai-7/operator-demo/operetor.js
+++ bai-7/operator-demo/operetor.js
@@ -1,2 +1,12 @@
-let inputWeight;
-let inputHeight;
+let inputWeight=prompt("Nhập vào chiều rộng")
+let inputHeight=prompt("Nhập vào chiều cao")
+let weight= parseInt(inputWeight)
+let height= parseInt(inputHeight)
+while (height<0||weight<0)
+{
+alert("Số không hợp lệ, nhập lại!!!")
+    inputWeight=prompt("Nhập vào chiều rộng")
+    inputHeight=prompt("Nhập vào chiều cao")
+}
+let dientich=weight*height;
+document.write("Diện tích là:"+dientich)
