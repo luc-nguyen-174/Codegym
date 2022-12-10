58585617a4f9e5875db42751bd04fbcd9e600254
luc-nguyen'174 <louiskingvn@gmail.com>
2022-12-10 21:46:33 +0700

    baitap

---
+++ bai-7/baitap-bien&kieudulieu/bien-kieudulieu.js
@@ -0,0 +1,20 @@
+function diemtb()
+{
+    let a1=document.getElementById("dvatly");
+    let a2=document.getElementById("dhoahoc");
+    let a3=document.getElementById("dsinhhoc");
+    let dtb=(parseInt(a1.value)+parseInt(a2.value)+parseInt(a3.value))/3
+    alert("Điểm trung bình là:"+dtb.toFixed(2));
+}
+function tinhtong()
+{
+    let a1=document.getElementById("dvatly");
+    let a2=document.getElementById("dhoahoc");
+    let a3=document.getElementById("dsinhhoc");
+    let tongdiem=parseInt(a1.value)+parseInt(a2.value)+parseInt(a3.value)
+    alert("Tổng điểm là:"+tongdiem);
+}
+
+function bai2(){}
+function bai3(){}
+function bai4(){}
