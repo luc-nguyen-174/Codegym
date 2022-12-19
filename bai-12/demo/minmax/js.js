75e8ccdde3dacd4d22f675f64bba5b4a134a5010
luc-nguyen'174 <louiskingvn@gmail.com>
2022-12-19 22:49:12 +0700

    thuchanh

--- bai-12/demo/minmax/js.js
+++ bai-12/demo/minmax/js.js
@@ -1,31 +1,46 @@
-//
-
-let array= new Array();
+let array = Array();
+let i=0;
 function addItem() {
-    let i=document.getElementById("txtValue").value;
-    let a=array.push(i);
-    document.getElementById("alert1").innerHTML="Gia tri " +i+ " da duoc gan vao o thu " + a;
+    array[i] = parseInt(document.getElementById("txtValue").value);
+    document.getElementById("alert1").innerHTML = "Gia tri " + array[i] + " da duoc gan vao vi tri " + i;
     i++;
     console.log(array);
 }
 
 //tim min
 function minFind() {
-    let max=array[0];
-    let index=0;
-    for (let x=1; x<array.length; x++) {
-        if (array[x]<max) {
-            max=array[i];
-            index=x;
+    let min = array[0];
+    let index = 0;
+    for (let x = 1; x < array.length; x++) {
+        if (array[x] < min) {
+            min = array[i];
+            index = x;
         }
     }
-    document.getElementById("minAlert").innerHTML=max;
+    document.getElementById("minAlert").innerHTML = ("gia tri thap nhat trong mang da nhap la:"+min+"tai vi tri"+index);
 }
 
 
 //tim max
 function maxFind() {
-
-
-
+    let max = array[0];
+    let index1 = 0;
+    for (let y = 1; y < array.length; y++) {
+        if (array[y] > max) {
+            max = array[i];
+            index1 = y;
+        }
+    }document.getElementById("maxAlert").innerHTML = ("gia tri cao nhat trong mang da nhap la:"+max+"tai vi tri"+index1);
 }
+
+function keyMap(evt) {
+    switch (evt.keyCode){
+        case 13:
+            addItem();
+            break;
+    }
+}
+//
+// function docReady() {
+//     window.addEventListener("keydown", keyMap(event));
+// }
