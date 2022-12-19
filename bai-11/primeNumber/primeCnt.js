cacca1fc00e2d0dd3d9959e6870c8b06c5238285
luc-nguyen'174 <louiskingvn@gmail.com>
2022-12-19 23:05:13 +0700

    baitap

--- bai-11/primeNumber/primeCnt.js
+++ bai-11/primeNumber/primeCnt.js
@@ -1,22 +1,19 @@
-function checkNT(number) {
-    let count = 0; // đếm số ước của số đang xét;
-    let countSNT = 0; // đếm xem đã đủ số lượng số nguyên tố cần in ra chưa;
-    for (let i = 2; i < 1000; i++) { // 2,3,4,5,6,7,8,....,1000
-        if (countSNT < number) { // Nếu số lượng SNT in ra < so với số lượng yêu cầu
-            for (let j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    count += 1;
-                }
-            }
-            if (count == 2) {
-                document.write(i + "<br>"); //4
-                countSNT += 1; //1
-            }
-            count = 0;
-        } else {
+let number = +prompt("Nhập số lượng số nguyên tố: ");
+let count = 0;
+let n = 2;
+let array = [];
+while (count < number) {
+    let flag = true;
+    for (let j = 2; j < n; j ++) {
+        if(n % j == 0) {
+            flag = false;
             break;
         }
     }
+    if(flag) {
+        count ++;
+        array.push(n)
+    }
+    n++
 }
-let number = parseInt(prompt("Nhập n số nguyên tố đầu tiên: ")); //10
-checkNT(number);
+document.getElementById("result").innerHTML = array;
